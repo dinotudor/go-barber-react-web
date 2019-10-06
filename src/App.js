4b96c3e58a16bf682684b295f84594e0ce2e8a1a
Dino Tudor <dinotudor@gmail.com>
2019-10-06 15:42:33 +0200

    dependencies, eslint-plugin-import, history, folders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return <h1>Hello Barber</h1>
+  return <h1>Hello Barber</h1>;
 }
 export default App;
