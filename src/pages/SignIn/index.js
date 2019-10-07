4d2b412bf948b84194fd29eb2e9cdf5a4eca2dbc
Dino Tudor <dinotudor@gmail.com>
2019-10-07 18:49:48 +0200

    unform rocketseat to send data from login and sign up

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Gobaber logo" />
 
-      <form>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="email" />
+        <Input name="password" type="password" placeholder="password" />
 
         <button type="submit">Sign in</button>
         <Link to="/register">Sign up for a new account</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="Gobaber logo" />
 
-      <form>
-        <input type="text" placeholder="Full name" />
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="email" />
+        <Input name="password" type="password" placeholder="password" />
 
         <button type="submit">Create account</button>
         <Link to="/">I already have an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
