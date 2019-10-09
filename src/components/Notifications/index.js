dde9d402456c889ce76469e5f98ae11bfd9f7de9
Dino Tudor <dinotudor@gmail.com>
2019-10-09 18:46:38 +0200

    added Date FNS, toggle notifications, useState and useEffects to get notifications from DB

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,6 +1,9 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
 import { MdNotifications } from 'react-icons/md';
+import { parseISO, formatDistance } from 'date-fns';
+import { es } from 'date-fns/locale/es';
+
+import api from '~/services/api';
 
 import {
   Container,
@@ -11,39 +14,46 @@ import {
 } from './styles';
 
 export default function Notifications() {
+  const [visible, setVisible] = useState(false);
+  const [notifications, setNotifications] = useState([]);
+
+  useEffect(() => {
+    async function loadNotifications() {
+      const response = await api.get('notifications');
+
+      const data = response.data.map(notification => ({
+        ...notification,
+        timeDistance: formatDistance(
+          parseISO(notification.createdAt),
+          new Date(),
+          { addSuffix: true, locale: es }
+        ),
+      }));
+
+      setNotifications(data);
+    }
+    loadNotifications();
+  }, []);
+
+  function handleToggleVisible() {
+    setVisible(!visible);
+  }
+
   return (
     <Container>
-      <Badge hasUnread>
+      <Badge onClick={handleToggleVisible} hasUnread>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
 
-      <NotificationList>
+      <NotificationList visible={visible}>
         <Scroll>
-          <Notification unread>
-            <p>You have a new appointent</p>
-            <time>1 day ago</time>
-            <button type="button">Mark as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appointent</p>
-            <time>1 day ago</time>
-            <button type="button">Mark as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appointent</p>
-            <time>1 day ago</time>
-            <button type="button">Mark as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appointent</p>
-            <time>1 day ago</time>
-            <button type="button">Mark as read</button>
-          </Notification>
-          <Notification>
-            <p>You have a new appointent</p>
-            <time>1 day ago</time>
-            <button type="button">Mark as read</button>
-          </Notification>
+          {notifications.map(notification => (
+            <Notification key={notification._id} unread={!notification.read}>
+              <p>{notification.content}</p>
+              <time>{notification.timeDistance}</time>
+              <button type="button">Mark as read</button>
+            </Notification>
+          ))}
         </Scroll>
       </NotificationList>
     </Container>
