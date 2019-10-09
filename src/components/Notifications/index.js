866fe791c47c1d3b65f2760eb361f06374d3a545
Dino Tudor <dinotudor@gmail.com>
2019-10-09 21:14:51 +0200

    handleMarkRead, useMemo hook

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { MdNotifications } from 'react-icons/md';
 import { parseISO, formatDistance } from 'date-fns';
 import { es } from 'date-fns/locale/es';
@@ -16,6 +16,11 @@ import {
 export default function Notifications() {
   const [visible, setVisible] = useState(false);
   const [notifications, setNotifications] = useState([]);
+
+  const hasUnread = useMemo(
+    () => notifications.find(notification => notification.read === false),
+    [notifications]
+  );
 
   useEffect(() => {
     async function loadNotifications() {
@@ -39,9 +44,19 @@ export default function Notifications() {
     setVisible(!visible);
   }
 
+  async function handleMarkAsRead(id) {
+    await api.put(`notifications/${id}`);
+
+    setNotifications(
+      notifications.map(notification =>
+        notification._id === id ? { ...notification, read: true } : notification
+      )
+    );
+  }
+
   return (
     <Container>
-      <Badge onClick={handleToggleVisible} hasUnread>
+      <Badge onClick={handleToggleVisible} hasUnread={hasUnread}>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
 
@@ -51,7 +66,14 @@ export default function Notifications() {
             <Notification key={notification._id} unread={!notification.read}>
               <p>{notification.content}</p>
               <time>{notification.timeDistance}</time>
-              <button type="button">Mark as read</button>
+              {!notification.read && (
+                <button
+                  type="button"
+                  onClick={() => handleMarkAsRead(notification._id)}
+                >
+                  Mark as read
+                </button>
+              )}
             </Notification>
           ))}
         </Scroll>
