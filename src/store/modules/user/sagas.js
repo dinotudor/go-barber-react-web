24d6a045f8393abfb01a18041b0d8692c9dd93cc
Dino Tudor <dinotudor@gmail.com>
2019-10-09 23:17:43 +0200

    update profile, saga, reducer and actions

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,25 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+
+    const profile = rest.oldPassword ? { ...payload.data } : { name, email };
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Profile succesfully updated');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.error('Profile not updated, please check you information');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
