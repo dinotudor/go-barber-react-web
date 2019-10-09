d6c108ac44bb8b97c0199845c384184279ec7443
Dino Tudor <dinotudor@gmail.com>
2019-10-09 16:39:36 +0200

    styles to notifications component

--- src/components/Notifications/index.js
+++ src/components/Notifications/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { MdNotifications } from 'react-icons/md';
 
-import { Container, Badge } from './styles';
+import { Container, Badge, NotificationList, Notification } from './styles';
 
 export default function Notifications() {
   return (
@@ -10,6 +10,24 @@ export default function Notifications() {
       <Badge hasUnread>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
+
+      <NotificationList>
+        <Notification unread>
+          <p>You have a new appointent</p>
+          <time>1 day ago</time>
+          <button type="button">Mark as read</button>
+        </Notification>
+        <Notification>
+          <p>You have a new appointent</p>
+          <time>1 day ago</time>
+          <button type="button">Mark as read</button>
+        </Notification>
+        <Notification>
+          <p>You have a new appointent</p>
+          <time>1 day ago</time>
+          <button type="button">Mark as read</button>
+        </Notification>
+      </NotificationList>
     </Container>
   );
 }
--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -24,3 +24,27 @@ export const Badge = styled.button`
       }
     `}
 `;
+
+export const NotificationList = styled.div`
+  position: absolute;
+  width: 260px;
+  left: calc(50% - 130px);
+  top: calc(100% + 30px);
+  background: rgba(0, 0, 0, 0.6);
+  border-radius: 4px;
+  padding: 20px;
+
+  &::before {
+    content: '';
+    position: absolute;
+    left: calc(50% - 20px);
+    top: -20px;
+    width: 0;
+    height: 0;
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-bottom: 20px solid rgba(0, 0, 0, 0.6);
+  }
+`;
+
+export const Notification = styled.div``;
