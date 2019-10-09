c039d3b514c4a09b73ff57d10a00b9bbcc60a063
Dino Tudor <dinotudor@gmail.com>
2019-10-09 13:03:24 +0200

    set token to validate requests

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -3,6 +3,6 @@ import { all } from 'redux-saga/effects';
 import auth from './auth/sagas';
 import user from './user/sagas';
 
-export default function* rootSafa() {
+export default function* rootSaga() {
   return yield all([auth, user]);
 }
