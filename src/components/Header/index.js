adf5f24e7024d7b7fd4bb5173c0dbaaf3cbf4354
Dino Tudor <dinotudor@gmail.com>
2019-10-11 14:06:40 +0200

    header dynamic

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import { Link } from 'react-router-dom';
 
 import Notifications from '~/components/Notifications';
@@ -8,6 +10,8 @@ import logoPurple from '~/assets/logo-small.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -19,12 +23,15 @@ export default function Header() {
           <Notifications />
           <Profile>
             <div>
-              <strong>Carol</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">My profile</Link>
             </div>
             <img
-              src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Carol"
+              src={
+                profile.avatar.url ||
+                'https://api.adorable.io/avatars/50/abott@adorable.png'
+              }
+              alt={profile.name}
             />
           </Profile>
         </aside>
