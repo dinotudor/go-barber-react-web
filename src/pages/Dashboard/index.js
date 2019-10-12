36f663d718938c7ac9e17c725bbe975b9e981239
Dino Tudor <dinotudor@gmail.com>
2019-10-12 16:14:43 +0200

    add style to dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('appointments');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 Setembro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Dino Tudor</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Free slot</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Dino Tudor</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Dino Tudor</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
